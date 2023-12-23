7b23cc1a1f1037fa527a6af47ebfd1ccb9a16765
thdlrt <1040880153@qq.com>
2023-12-23 21:50:05 +0800

    init for linux

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,28 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
-cd "$(dirname "${BASH_SOURCE}")";
+cd "$(dirname "${0}")"
 
-git pull origin main;
+git pull origin main
 
-function doIt() {
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".osx" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
-		-avh --no-perms . ~;
-	source ~/.bash_profile;
+doIt() {
+    rsync --exclude ".git/" \
+        --exclude ".DS_Store" \
+        --exclude ".osx" \
+        --exclude "bootstrap.sh" \
+        --exclude "README.md" \
+        --exclude "LICENSE-MIT.txt" \
+        -avh --no-perms . ~
+    source ~/.zshrc
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+if [[ "$1" == "--force" || "$1" == "-f" ]]; then
+    doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
-unset doIt;
+    read "REPLY?This may overwrite existing files in your home directory. Are you sure? (y/n) "
+    echo ""
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        doIt
+    fi
+fi
+unset doIt
+
